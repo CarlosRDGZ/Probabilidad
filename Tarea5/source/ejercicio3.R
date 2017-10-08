c380e5c5d9475811befc621d931fe130ebcdbdc1
Carlos Francisco Rodríguez Aguilar <crodriguez20@ucol.mx>
2017-10-08 17:59:02 -0500

    Graph 1.3

--- Tarea5/source/ejercicio3.R
+++ Tarea5/source/ejercicio3.R
@@ -29,6 +29,10 @@ plot(x = ps, y = ns[1,], type = "l", col = colors[1],
 for (i in 2:length(Ns))
     lines(x = ps, y = ns[i,], type="l", col=colors[i])
 
+# legend("topleft",
+# 	legend = c("N = 7000", "N = 5000", "N = 3000", "N = 2000", "N = 100", "N = 300"),
+# 	xpd = TRUE, inset = c(0, 0), bty = "n", fill = colors, cex = 1)
+
 legend("topleft",
-	legend = c("N = 7000", "N = 5000", "N = 3000", "N = 2000", "N = 100", "N = 300"),
-	xpd = TRUE, inset = c(0, 0), bty = "n", fill = colors, cex = 1)
+       legend = c("N = 7000", "N = 5000", "N = 3000", "N = 2000", "N = 100", "N = 300"),
+       cex=0.5, fill = colors, ncol = 2)
--- Tarea5/source/ejercicio4.R
+++ Tarea5/source/ejercicio4.R
@@ -12,23 +12,23 @@ if (file.exists("formulas.R")) {
 }
 # ------------------------------ #
 
-Ns <- seq(100, 800, 10)
+Ns <- c(7000, 5000, 3000, 2000, 100, 300)
 Z <- ZValue(0.95)
-ds <- c(0.01, 0.03, 0.05, 0.06, 0.07)
+ds <- seq(0.0001, 0.2, 0.00001)
 p <- 0.05
-ns <- matrix(NA, nrow = length(ds), ncol = length(Ns))
+ns <- matrix(NA, nrow = length(Ns), ncol = length(ds))
 
-for (i in 1:length(ds))
-	for(j in 1:length(Ns))
-		ns[i, j] = SampleSize(Ns[j], Z, p, ds[i])
+for (i in 1:length(Ns))
+	for(j in 1:length(ds))
+		ns[i, j] = SampleSize(Ns[i], Z, p, ds[j])
 
 colors <- c("#b82c2c", "#066fa9", "#62b7a7", "#f36757", "#ffc87c", "#f8971c")
 
-plot(x = ds, y = ns[, 1] , type = "l", col = colors[1],
+plot(x = ds, y = ns[1, ] , type = "l", col = colors[1],
      xlab = "Valores del margen de error (d)", ylab = "Tamanio de la muestra (n)")
-for (i in 2:length(ds))
-    lines(x = ds, y = ns[ ,i], type="l", col=colors[i])
+for (i in 2:length(Ns))
+    lines(x = ds, y = ns[i, ], type="l", col=colors[i])
 
-legend("bottomleft",
+legend("topright",
 	legend = c("N = 7000", "N = 5000", "N = 3000", "N = 2000", "N = 100", "N = 300"),
-	cex=0.5, fill = colors, ncol = 2)
+	cex=0.3, fill = colors, ncol = 2)
